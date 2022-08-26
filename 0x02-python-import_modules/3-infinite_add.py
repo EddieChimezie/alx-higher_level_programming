a7c5fff8966243e8cb9fd4b3d67a75990605649d
Edison Chimezie <edison.chimezie01@gmail.com>
2022-08-25 17:23:52 -0700

    Addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    if (len(sys.argv) > 1):
-        for i in range(1, len(sys.argv)):
-            result += (int(sys.argv[i]))
-    print("{:d}".format(result))
-
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
